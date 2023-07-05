f4af28ec8cdfa515d6bfdf52c369d3e1deb80f2f
PC <luisrenegonzalezpe@gmail.com>
2023-07-05 12:49:09 -0500

    portafolio final

--- validacion.js
+++ validacion.js
@@ -1,1 +1,30 @@
-//Haz tú validación en javascript acá
+function validateForm() {
+    var nombre = document.getElementById("nombre").value;
+
+    var email = document.getElementById("email").value;
+
+    var asunto = document.getElementById("asunto").value;
+
+    var mensaje = document.getElementById("mensaje").value;
+
+        if (nombre == "") {
+            alert ("Por favor, ingresa tu nombre");
+            return false;
+        }
+
+        if (email == "") {
+            alert ("Por favor, ingresa tu email");
+            return false
+        }
+
+        if (asunto == "") {
+            alert("Por favor, ingresa el asunto");
+        }
+
+        if (mensaje == "") {
+            alert("Por favor, ingresa tu mensaje");
+            return false;
+        }
+
+        return true
+}
